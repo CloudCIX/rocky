7ebabc57132e5b412f886d0878adea541195015d
Jerry Sweeney <js@cix.ie>
2019-12-20 11:42:28 +0000

    Update settings-template.py

--- settings-template.py
+++ settings-template.py
@@ -2,14 +2,6 @@
 #  This file needs to be modified and renamed settings.py before use.  #
 #  ######################################################################
 
-"""python-cloudcix api settings"""
-
-CLOUDCIX_API_URL = 'https://api.your_url.com/'
-CLOUDCIX_API_USERNAME = 'rocky@your_url.com'
-CLOUDCIX_API_PASSWORD = 'Password_Here'
-CLOUDCIX_API_KEY = 'Get_From_Membership_App'
-CLOUDCIX_API_VERSION = 2
-CLOUDCIX_API_V2_URL = 'https://api.your_url.com/'
 
 #  Rocky Version
 VERSION = "0.2.0"
@@ -18,19 +10,44 @@ VERSION = "0.2.0"
 ROUTER_NAME_SERVERS = ["91.103.0.1", "91.103.0.2", "8.8.8.8", "2a02:2078:cb01::2"]
 
 #  Public Keys
-# These Keys allow Rocky and Robot to change the network configuration on the  SRX Routers 
-ROBOT_RSA = "All instances of Robot have the same key pair. Place the public key here "
+# These Keys allow Rocky to change the network configuration on the SRX Routers 
 ROCKY_RSA = "Place Rocky's public key here"
 
-#  Access list
-# The management network is controlled by SRX routers and is firewall protected, so all inbound traffic is blocked.
-# An allow policy with the access list ips can enable access to the management network from the outside world.
-# The access list can be API servers, deployment servers, etc. where the management hosts (for example,  Robot) need to
-# communicate with servers in access list.
-# The format is {'name of server': 'ip address/range', ...etc}
+#  Managed Cloud Infrastructure
+# One instance of Rocky can manage many Clouds and each Cloud can have many regions.
+# This configuration describes the Cloud infrastructure managed by this instance of Rocky.
 
-MGMT_ACCESS_LIST = {
-    "example-management-server": "ip address / range",
-    "example-application-server": "ip address / range",
-    "rocky-server": "ip address / range"
-}
+clouds  = [
+    # First Cloud ...
+    {
+        'name': 'First Cloud',
+        
+        # python-cloudcix api settings
+        'CLOUDCIX_API_URL' :'https://api.your_first_cloud_url.com/',
+        'CLOUDCIX_API_USERNAME': 'rocky@your_first_cloud_url.com',
+        'CLOUDCIX_API_PASSWORD': 'Password_Here',
+        'CLOUDCIX_API_KEY': 'Get_From_Membership_App',
+        'CLOUDCIX_API_VERSION': 2,
+        'CLOUDCIX_API_V2_URL': 'https://api.your_first_cloud_url.com/',
+        
+         # SRX settings
+         # These Keys allow Robot to change the network configuration on the SRX Routers 
+        'ROBOT_RSA': "All instances of Robot have the same key pair. Place the public key here ",
+         # Inbound Access list
+         # The management network of this SRXPod is firewalled to block all inbound traffic.
+         # Access is allowed to the management network from this list of external Public IPs.
+         #  If this region is managed by an external COP then the IP address ranges of that COP must be allowed in.
+         #  If this region contains a COP then evternal access is not required and leave these
+         #  This region must be monitored by a instance of Rocky and therefore the IP address
+         'MGMT_ACCESS_LIST': {
+             'COP_IPV4' : '255.255.255.255/32',
+             'COP_IPV6' : 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff/128',
+             'ROCKY': 'Rocky ipaddress'
+         },
+    },
+    
+    # Second Cloud ...
+    {
+        "name": 'Second Cloud',
+    },
+]
