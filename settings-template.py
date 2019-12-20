287b3591d23d45077af25bc5e7214936e2ec0351
Jerry Sweeney <js@cix.ie>
2019-12-20 12:17:48 +0000

    Update settings-template.py

--- settings-template.py
+++ settings-template.py
@@ -30,20 +30,63 @@ clouds  = [
         'CLOUDCIX_API_VERSION': 2,
         'CLOUDCIX_API_V2_URL': 'https://api.your_first_cloud_url.com/',
         
-         # SRX settings
-         # These Keys allow Robot to change the network configuration on the SRX Routers 
-        'ROBOT_RSA': "All instances of Robot have the same key pair. Place the public key here ",
-         # Inbound Access list
-         # The management network of this SRXPod is firewalled to block all inbound traffic.
-         # Access is allowed to the management network from this list of external Public IPs.
-         #  If this region is managed by an external COP then the IP address ranges of that COP must be allowed in.
-         #  If this region contains a COP then evternal access is not required and leave these
-         #  This region must be monitored by a instance of Rocky and therefore the IP address
-         'MGMT_ACCESS_LIST': {
+        # Region settings
+        'region': [
+            # First region
+            {
+                'name': 'First Region',
+                
+                # Network schema
+                'IPv4': [
+                    {
+                        'address_range': 'a.b.c.d/e',
+                        'gateway': 'a.b.c.x',
+                    },
+                    {
+                        'address_range': 'a.b.c.d/e',
+                        'gateway': 'a.b.c.x',
+                    },
+                ],
+                
+            },
+            
+            # Second region
+            {
+                'name': 'Second Region'
+            },
+        ],
+        
+        # SRX settings
+        # SSH Keys allow Robots in each region to configure local SRX Routers. 
+        'ROBOT_RSA': "All instances of Robot in a Cloud have the same key pair. Place the public key here ",
+        
+        # Inbound Access list
+        # The management network of this SRXPod is firewalled to block all inbound traffic.
+        # Access is allowed to the management network from this list of external Public IPs.
+        'MGMT_ACCESS_LIST': {
+             #  If this region is managed by an external COP then the IP address ranges of that COP must be allowed in.
+             #  If this region contains a COP then evternal access is not required and leave these
              'COP_IPV4' : '255.255.255.255/32',
              'COP_IPV6' : 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff/128',
+             
+             #  This region must be monitored by a instance of Rocky and therefore the IP address
              'ROCKY': 'Rocky ipaddress'
-         },
+        },
+        
+        # VPN Tunnel from Support Center to the SRXPOD for management.
+        'vpns': [],
+        
+        # A minimum of /29 IPv4 floating gateway subnet for COP servers
+        'IPv4': {
+            'address_range': 'a.b.c.d/e',
+            'gateway': 'a.b.c.x',
+        },
+        
+        # A /48 IPv6 routed subnet for Management and Projects
+        'IPv6': {
+            'address_range': 'A:B:C::/48',
+            'link_subnet': 'X:Y:z::/N',  #  /126 per region minimum
+        },
     },
     
     # Second Cloud ...
